ace1c2adaaa4992edc007b92b38f1b255f29c453
nathaliabrazk <nathaliabrazk@gmail.com>
2023-01-19 23:09:39 -0300

    .

--- ex04.py
+++ ex04.py
@@ -1,6 +1,6 @@
 #program that reads data from the keyboard and returns its primitive type and all possible information about it 
 s = input('Type something: ')#s in thits case its a object
-print( 'The primite type is:',type(s) )#if primitive type isnt declared the program allways will return str
+print( 'The primitive type is:',type(s) )#if primitive type isnt declared the program allways will return str
 print( 'Its a number?', s.isnumeric() )
 print( 'Its a letter?', s.isalpha() )
 print( 'Its alphanumeric?', s.isalnum())
