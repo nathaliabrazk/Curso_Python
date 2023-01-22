9a22adfdff52ea355e9e168701da92983ab5adf3
nathaliabrazk <nathaliabrazk@gmail.com>
2023-01-22 01:45:11 -0300

    .

---
+++ ex10.py
@@ -0,0 +1,4 @@
+#program who read how much money a people have on a wallet and show how much dollars he can buy
+m = int(input( 'Type the amount of money:' ))
+d = m*5.21
+print('You can buy {} dollars'.format(d))
---
+++ ex11.py
@@ -0,0 +1,1 @@
+#program who read the width and the heigh of a wall in meters, calculate your area and the amount of tint necessary to paint there, knowing that eacher liter, paint an area 2m2
