42b84b77f1b97fbc66788c0b4d916789e9792cac
nathaliabrazk <nathaliabrazk@gmail.com>
2023-01-21 23:04:46 -0300

    Update aula07.py

---
+++ aula07.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um numero:'))
+n2 = int(input('Digite outro numero'))
+s = n1+n2
+m = n1*n2
+di = n1//n2
+po = n1**n2
+print( 'A soma é {}, o produto é {}, a divisao é {}'.format(s,m,di) )
+print( 'Divisão inteira é {}, potencia é {}'.format(di,po))
