4db52ee8d84c31a3667dd367d80c822ee70f5f5e
nathaliabrazk <nathaliabrazk@gmail.com>
2023-01-23 00:51:50 -0300

    .

--- ex05.py
+++ ex05.py
@@ -1,5 +1,3 @@
-#program that reads a numner and shows int predecessor and successor
+#program that reads a int number and shows your predecessor and successor
 n = int(input( 'Type a number:' ))
-a = n-1
-s = n+1
-print( 'Predecessor:{}\nSucessor:{}'.format(a,s) )
+print( 'Number:{}\nPredecessor:{}\nSucessor:{}'.format(n,(n-1),(n+1)))
--- ex06.py
+++ ex06.py
@@ -1,7 +1,12 @@
 #Program who reads a number and show your double, the triple and your square root
-import math
-n = int(input( 'Type a number:' ))
-d = n**2
-t = n**3
-r = math.sqrt(n)
-print( 'The double is {}\nThe triple is {}\nThe square root is {}'.format(d,t,r))
+n = int(input('Type a number:'))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print( 'Number:{}\nDouble:{}\nTriple:{}\nSquare root:{}'.format(n,d,t,r))
+#import math
+#n = int(input( 'Type a number:' ))
+#d = n**2
+#t = n**3
+#r = math.sqrt(n)
+#print( 'The double is {}\nThe triple is {}\nThe square root is {}'.format(d,t,r))
