7ddd666897b20a6b142ffbe4ae7ce330fc290b98
nathaliabrazk <nathaliabrazk@gmail.com>
2023-01-23 23:00:21 -0300

    Update ex07.py

--- ex07.py
+++ ex07.py
@@ -2,4 +2,4 @@
 n1 = int(input( 'Type the first note:' ))
 n2 = int(input( 'Type the second note: '))
 m = (n1+n2)/2
-print('The average is:{}'.format(m))
+print('The average of {} and {} is:{}'.format(n1,n2,m))
