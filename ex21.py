f5c31fe53cc56f58980d61d046707f5ebf7153c3
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-01-30 20:25:43 -0300

    .

--- ex21.py
+++ ex21.py
@@ -1,6 +1,4 @@
 #Program who play audio from an mp3 file
 from playsound import playsound
-playsound.init()
-playsound.mixer.music.load('ex21.mp3')
-playsound.mixer.music.play()
-playsound.event.wait()
+playsound('ex21.mp3')
+
