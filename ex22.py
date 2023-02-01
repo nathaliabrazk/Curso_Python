b3bfd90afff07aec62b76b3dbd7bd6895d103cb8
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-01 17:58:51 -0300

    .

---
+++ ex22.py
@@ -0,0 +1,7 @@
+#Program will read a full name and show: the name in all capital letters , the name in all lowercase letters, 
+#number of letters not counting spaces
+name = input('Type a full name:')
+print(name.upper())
+print(name.lower())
+print(name.split())
+print(len(name))
