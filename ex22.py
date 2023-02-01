9bd50b2da2897933e94f49261787d57555295185
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-01 18:22:31 -0300

    .

--- ex22.py
+++ ex22.py
@@ -1,7 +1,7 @@
 #Program will read a full name and show: the name in all capital letters , the name in all lowercase letters, 
 #number of letters not counting spaces
-name = input('Type a full name:')
-print(name.upper())
-print(name.lower())
-print(name.split())
-print(len(name))
+name = str(input('Type a full name:')).strip()
+print('Name in all uppercase letters: ', name.upper())
+print('Name in all lowercase letters:', name.lower())
+print('Name has:', len(name) - name.count(' '), 'letters')
+print('The first name has', name.find(' '), 'letters')
