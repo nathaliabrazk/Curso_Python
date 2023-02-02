b08d994cef7051a929e55f273af79ff9ee959813
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-02 20:57:44 -0300

    .

---
+++ ex32.py
@@ -0,0 +1,18 @@
+#Program will read 3 numbers and show wich is the biggest and wich is the smallest
+n1 = int(input('Type a number:'))
+n2 = int(input('Type a number:'))
+n3 = int(input('Type a number:'))
+#big
+if n1 > n2 and n3:
+    print('The biggest number:{}'.format(n1))
+elif n2 > n1 and n3:
+    print('The biggest number:{}'.format(n2))
+elif n3 > n1 and n2:
+    print('The biggest number:{}'.format(n3))
+#small
+if n1 < n2 and n3:
+    print('The smalest number:{}'.format(n1))
+elif n2 < n1 and n3:
+    print('The smalest number:{}'.format(n2))
+elif n3 < n1 and n2:
+    print('The smalest number:{}'.format(n3))
