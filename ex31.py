3094ee27cdd5184a1cb34d9ff2a7dcb9aa2d8b53
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-02 20:34:54 -0300

    .

---
+++ ex31.py
@@ -0,0 +1,7 @@
+#Program will ask the distace of a trip in km. Calculate the price from the ticket.
+#Charging R$0,50 for km to trips from to 200 km and R$0,45 to more longers trips
+distance = int(input('Type the distance of a trip in km:'))
+if distance <= 200:
+    print('Ticket value: R$ {}'.format(distance * 0.50))
+else:
+    print('Ticket value: R$ {}'.format(distance * 0.45))
