2615bf7cc1a5ef7cbb0227449b53b5495a8d7fb5
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-01 23:28:21 -0300

    .

---
+++ ex26.py
@@ -0,0 +1,7 @@
+#Program who read a phrase and show:
+#*How many times does the letter "A" appears;
+#*What position does it appear in the first time;
+#*What position does it appear in the last time.
+phrase = str(input('Type a phrase:')).strip().upper()
+print('The letter "A" apears: {} times'.format(phrase.count('A')))
+print('The position where "A" first appears:{}'.format(phrase.find('A'[0])))
