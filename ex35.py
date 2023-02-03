6c4eb647981761227f172427fe1032cdb5ccc63b
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-03 19:45:41 -0300

    .

--- ex35.py
+++ ex35.py
@@ -1,7 +1,7 @@
 #Program will read a lenght of three straight lines and say to the user if they can or not form a triangle
-sl1 = int(input('Type a lenght of a straight line:'))
-sl2 = int(input('Type a lenght of a straight line:'))
-sl3 = int(input('Type a lenght of a straight line:'))
+sl1 = int(input('First straight line:'))
+sl2 = int(input('Second lenght of a straight line:'))
+sl3 = int(input('Third lenght of a straight line:'))
 if sl1 + sl2 > sl3:
     print('Can form a triangle!')
 elif sl1 + sl3 > sl2:
