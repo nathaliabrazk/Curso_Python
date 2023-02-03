ef42692c6987fedf002ffe1642e707243aa6a42e
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-02 21:15:55 -0300

    .

--- ex32.py
+++ ex32.py
@@ -3,16 +3,16 @@ n1 = int(input('Type a number:'))
 n2 = int(input('Type a number:'))
 n3 = int(input('Type a number:'))
 #big
-if n1 > n2 and n3:
+if n1 > n2 and n1 > n3:
     print('The biggest number:{}'.format(n1))
-elif n2 > n1 and n3:
+elif n2 > n1 and n2 > n3:
     print('The biggest number:{}'.format(n2))
-elif n3 > n1 and n2:
+elif n3 > n1 and n3 > n2:
     print('The biggest number:{}'.format(n3))
 #small
-if n1 < n2 and n3:
+if n1 < n2 and n1 < n3:
     print('The smalest number:{}'.format(n1))
-elif n2 < n1 and n3:
+elif n2 < n1 and n2 < n3:
     print('The smalest number:{}'.format(n2))
-elif n3 < n1 and n2:
+elif n3 < n1 and  n3 < n2:
     print('The smalest number:{}'.format(n3))
