4e25dd9eea5d51a8e35e013e6dfc1ad2e3eac1f8
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-02 21:35:52 -0300

    .

--- ex32.py
+++ ex32.py
@@ -1,18 +1,6 @@
-#Program will read 3 numbers and show wich is the biggest and wich is the smallest
-n1 = int(input('Type a number:'))
-n2 = int(input('Type a number:'))
-n3 = int(input('Type a number:'))
-#big
-if n1 > n2 and n1 > n3:
-    print('The biggest number:{}'.format(n1))
-elif n2 > n1 and n2 > n3:
-    print('The biggest number:{}'.format(n2))
-elif n3 > n1 and n3 > n2:
-    print('The biggest number:{}'.format(n3))
-#small
-if n1 < n2 and n1 < n3:
-    print('The smalest number:{}'.format(n1))
-elif n2 < n1 and n2 < n3:
-    print('The smalest number:{}'.format(n2))
-elif n3 < n1 and  n3 < n2:
-    print('The smalest number:{}'.format(n3))
+#Program will read a any year and show if he is bissextile
+year = int(input('Type a year:'))
+if year % 4 == 0:
+    print('Bissextile year!')
+else:
+    print('Not bissextile year!')
