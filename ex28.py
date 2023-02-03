43083fee0d0d5e6ed0f77059685678bb964f85c2
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-02 22:25:45 -0300

    .

--- ex28.py
+++ ex28.py
@@ -2,10 +2,15 @@
 #discover what the number was choiced by the computer
 #The computer should write in the screen if the user won or lost
 import random
-number = random.randint(0,5)
-print(number)
-numberUser = int(input('Type a number between 0 and 5:'))
-if number == numberUser:
-    print('You won, congratulations!')
+from time import sleep
+print('-=-' * 20)
+print('Im gonna think of a number between 0 and 5, try to guess :)')
+print('-=-' * 20)
+computer = random.randint(0,5)
+user = int(input('What number between 0 and 5 did i think of? '))#user tries to guess
+print('PROCESSING...')
+sleep(3)
+if computer == user:
+    print('You won, CONGRATULATIONS!')
 else:
-    print('You lost!')
+    print('You lost!\nI tought of the number:{}\nGood luck in the next!'.format(computer))
---
+++ ex35.py
@@ -0,0 +1,10 @@
+#Program will read a lenght of three straight lines and say to the user if they can or not form a triangle
+sl1 = int(input('Type a lenght of a straight line:'))
+sl2 = int(input('Type a lenght of a straight line:'))
+sl3 = int(input('Type a lenght of a straight line:'))
+if sl1 + sl2 > sl3:
+    print('Can form a triangle!')
+elif sl1 + sl3 > sl2:
+    print('Can form a triangle!')
+elif sl2 + sl3 > sl1:
+    print('Can form a triangle!')
