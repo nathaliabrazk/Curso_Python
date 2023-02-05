873590cba903cc62de15daee278eb6c2a34237fe
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-05 00:05:49 -0300

    .

--- ex44.py
+++ ex44.py
@@ -15,3 +15,12 @@ elif option == 2:
     print('You will pay in cash on card, so you have 5% of discount!')
     discount = (5 * price/ 100)
     print('The value of R${} with discount is: R${}'.format(price, price - discount))
+elif option == 3:
+    print('You will pay in until 2x on the card, so you go pay the normal price!')
+    print('The purchase price:{}'.format(price))
+elif option == 4:
+    print('You will pay 3x or more on the card, so you have 20% of discount!')
+    discount = (20 * price / 100)
+    print('The value of R${} with discount is R${}'.format(price, price - discount))
+else:
+    print('Type just (1, 2, 3 or 4!)')
