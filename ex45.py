a96040d314d109f84c077cbc65bfbf19e8bb9e84
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-05 23:49:19 -0300

    .

--- ex45.py
+++ ex45.py
@@ -1,13 +1,13 @@
 #Program that plays jokenpô with you
 from random import randint
-itens = ('Stone', 'Paper, 'Scissors')
+itens = ('Stone', 'Paper', 'Scissors')
 computer = randint(0, 2)
-print('-*-') * 20
-print('             JOKENPÔ MENU')
-print('-*-') * 20
+print('-*-' * 20) 
+print('                        JOKENPÔ MENU')
+print('-*-' * 20)
 print('Stone - [1]')
 print('Paper - [2]')
 print('Scissors - [3]')
 player = int(input('Type your move:'))
-print('Computer move:{}',itens[computer])
-print('Player move:{}',itens[computer])
+print('Computer move:{}'.format(itens[computer]))
+print('Player move:{}'.format(itens[computer]))
