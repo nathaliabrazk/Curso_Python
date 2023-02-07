2d5f57294e6a64e0c3bebf3b89e89a228f1e6000
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-07 00:06:58 -0300

    .

--- ex46.py
+++ ex46.py
@@ -1,5 +1,7 @@
 #Program who read show in the screen the countdown to the burst of fireworks goin from 10 to 0
 #with a break of 1 second between they 
+from time import sleep
 for count in range(10, 0-1, -1):
     print(count)
+    sleep(0.5)
 print('BOOM!')
