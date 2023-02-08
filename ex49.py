108e20fb9827e92360d6e35d2ea98fc93d40a67e
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-07 23:41:52 -0300

    .

--- ex49.py
+++ ex49.py
@@ -1,7 +1,6 @@
 #Remake the ex 09, showing in the screen the multiplication table of a number who the user choice 
 #using a loop forn 
 count = 0
-n = int(input('Type a number:'))
-for n in range(1,101):
-    sum = count + 1
-    print('{} x {}'.format(n, sum))
+n = int(input('Type a number to see your multiplication table:'))
+for c in range(1,11):
+    print('{} x {:2} = {}'.format(n, c, n*c))
