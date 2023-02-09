2bd7d5509728534a2a2e29040dfbf33d7d8107b0
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-08 22:59:18 -0300

    .

---
+++ ex51.py
@@ -0,0 +1,8 @@
+#Program who read the first term and the reason of a P.A. In the end show the 10 first terms 
+#of this progression
+term = int(input('Type the first term:'))
+reason = int(input('Type the reason:'))
+tenth = term + (10 - 1) * reason
+for c in range(term,tenth,reason):
+    print(' {} '.format(c), end=' -> ')
+print('end')
