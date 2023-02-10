38435a195eae2e0432007cc7e49d190e57569d22
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-09 22:39:23 -0300

    .

--- ex52.py
+++ ex52.py
@@ -1,6 +1,9 @@
 #Program who read a whole number and say if he is or not a prime number
 n = int(input('Type a number:'))
-if n % 1 == 0 and n % n == 0:
-    print('{} is a PRIME NUMBER!'.format(n))
-else:
+total = 0
+for c in range(1, n + 1):
+ if c % 1 == 0 and c % c == 0:
+    total += 1
+ else:
     print('{} is not a prime number'.format(n))
+print('The number {} was divisible {} times'.format(n,total))
