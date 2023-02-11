5046f96df5abdd207dee4d542dd45e5bbc2039d0
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-10 23:06:12 -0300

    .

---
+++ ex54.py
@@ -0,0 +1,11 @@
+#Program who read the age of 7 people and show how many people have not yet reached the age of majority
+minor = 0
+major = 0
+lage = 0
+for c in range(1,8):
+    age = int(input('Type your age:'))
+    if age < 18:
+        minor += 1
+    else:
+        lage += 1
+print('{} people have not yet reached'.format(minor))
