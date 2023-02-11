ed947f43d843e20352289d2af035857568f5cc92
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-10 23:39:32 -0300

    .

---
+++ ex55.py
@@ -0,0 +1,14 @@
+#Program who read the weight of 5 people and show wich was the biggest and the smallest 
+bigger = 0
+smaller = 0
+for c in range(1, 6):
+    weight = float(input('Weight of the {}ª person:'.format(c)))
+    if c == 1:
+        bigger = weight
+        smaller = weight
+    if weight > bigger:
+        bigger = weight
+    if weight < smaller:
+        smaller = weight 
+print('The bigger weight is: {} kg'.format(bigger))
+print('The smaller weight is: {} kg'.format(smaller))
