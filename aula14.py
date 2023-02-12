13e68691b99c74c7d0fef85e3ae7a2cd6c156fb6
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-11 23:13:16 -0300

    .

---
+++ aula14.py
@@ -0,0 +1,13 @@
+#WHILE
+
+#repeating loop with FOR:
+#for c in range (1,10):
+#   print(c)
+#print('end')
+
+#repeating loop with while:
+c = 1
+while c < 10:
+    print(c)
+    c = c + 1
+print('end')
