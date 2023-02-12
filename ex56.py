a2b2e0c804f20962bf44df0a927cef0cf3690fd2
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-11 22:16:17 -0300

    .

--- ex56.py
+++ ex56.py
@@ -1,6 +1,6 @@
 #Program who read the name, age and sex from 4 people. In the end show: the average of age from the group
 #What's the name of the oldest man. How many women are under 20. 
 for c in range (1,5):
-    print('{}° name:'.format(c))
-    print('{}° age:'.format(c))
-    print('{}° sex:'.format(c))
+    name = str(input('{}° name:'.format(c)))
+    age = int(input(('{}° age:'.format(c))))
+    sex = str(input('{}° sex:'.format(c)))
