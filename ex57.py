ce2430e281ce4f17fca3290bc891693314401a7a
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-12 23:32:53 -0300

    .

--- ex57.py
+++ ex57.py
@@ -1,5 +1,14 @@
 #Program who read the sex of a person, but just acept the values 'M' or 'F'. In case of error ask for 
 #typing again until you have the correct reply
-sex = str(input('Type the sex [F/M]:')).strip().upper()[0]
-while sex not in 'FfMm':
-    sex = str(input('Invalid (Just type [F or M]):')).strip().upper()[0]
+##ex = str(input('Type the sex [F/M]:')).strip().upper()
+#while not sex == 'F' and not sex == 'M':
+#sex = str(input('Invalid (Just type [F or M]):')).strip().upper()
+#print('Sex {} registered with sucess!'.format(sex))
+s = str(input('Por favor, digite o sexo (M ou F): ')).upper().strip()[0]
+while s != 'F' and s != 'M':
+    s = str(input('Dados inválidos. Por favor, digite o sexo (M ou F): ')
+            ).upper().strip()[0]
+if s == 'F':
+    print('Sexo Feminino cadastrado.')
+if s == 'M':
+    print('Sexo Masculino cadastrado.')
