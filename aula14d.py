a1bf8d901f0ebbd5e7dfa2d947a271357c82a51d
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-12 22:59:42 -0300

    .

--- aula14d.py
+++ aula14d.py
@@ -5,8 +5,9 @@ odd = 0
 while n != 0:
     n = int(input('Type a number:'))
     print('If you want stop the program type 0.')
-    if n % 2 == 0:
-        pair += 1
-    else:
-        odd += 1
-print('You type {} pair numbers and {} odd numbers'.format(pair,odd))
+    if n != 0:
+        if n % 2 == 0:
+            pair += 1
+        else:
+            odd += 1
+print('You typeD {} pair numbers and {} odd numbers'.format(pair,odd))
