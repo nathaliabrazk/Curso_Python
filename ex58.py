62dec16b7fe5574afa981a374e58cf1978dac58f
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-13 23:02:19 -0300

    .

--- ex58.py
+++ ex58.py
@@ -2,6 +2,15 @@
 #until hit, showing in the end how many guesses were needed to get yhe right answer
 import random
 from time import sleep
+t = 1
+
 computer = random.randint(1,10)
 print('Im thinking in a number between 1 and 10')
 sleep(1)
+user = int(input('New try guess the number i think:'))
+sleep(1.5)
+while user != computer:
+    user = int(input('WRONG!\nTry again:'))
+    sleep(1.5)
+    t += 1
+print('YOU WIN!\nThe number of tryes you need to win:{}'.format(t))
