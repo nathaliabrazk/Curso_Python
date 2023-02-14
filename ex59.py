f2d99d28cfac6f8d2c0432ca6acd38db7805fe05
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-13 23:47:43 -0300

    .

---
+++ ex59.py
@@ -0,0 +1,11 @@
+#Program who reed two numbers and show the menu:
+#[1] add
+#[2] multiply
+#[3] bigger
+#[4] type new numbers 
+#[5] go out the program
+#Your program need perform the requeseted operation 
+print('=' * 28)
+print('            MENU')
+print('=' * 28)
+print('    [1] Add\n    [2] Multiply\n    [3] Bigger\n    [4] Type new numbers\n    [5] Go out the program')
