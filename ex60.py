b3121018e97b2fa3a2687d6c2dc0bbb13892771b
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-14 20:55:05 -0300

    .

--- ex60.py
+++ ex60.py
@@ -3,5 +3,5 @@ from math import factorial
 n = int(input('Type the number for show your factorial:'))
 factorial(n)
 while n > 1:
-    n * (n -1)
-print('Calculing {}!: {} = {}'.format(n, factorial))
+    n * (n - 1)
+print('Calculing {}! = {}'.format(n, factorial))
