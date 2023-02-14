af36035a74b991707c797b96baa77df224acca26
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-14 00:00:30 -0300

    .

--- ex59.py
+++ ex59.py
@@ -7,12 +7,18 @@
 #Your program need perform the requeseted operation
 n1 = int(input('Type the first number:'))
 n2 = int(input('Type the second number:'))
+
 print('=' * 28)
 print('            MENU')
 print('=' * 28)
 print('    [1] Add\n    [2] Multiply\n    [3] Bigger\n    [4] Type new numbers\n    [5] Go out the program')
 op = int(input('Type your choice:'))
-if op == 1:
-    print('You choice add!')
-    add = n1 + n2
-    print('The sum of {} and {} are: {}'.format(n1,n2,add))
+while op != 5:
+    if op == 1:
+        print('You choice add!')
+        add = n1 + n2
+        print('The sum of {} and {} are: {}'.format(n1,n2,add))
+    if op == 2:
+        print('You choice multiply!')
+        mult = n1 * n2
+        print('The multiply of {} and {} are: {}'.format(n1,n2,mult))
