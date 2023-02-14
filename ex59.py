8a79c2447c90ae570f94d8ef9db28cb7a299105b
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-14 20:01:58 -0300

    .

--- ex59.py
+++ ex59.py
@@ -14,19 +14,23 @@ print('=' * 28)
 print('    [1] Add\n    [2] Multiply\n    [3] Bigger\n    [4] Type new numbers\n    [5] Go out the program')
 op = int(input('Type your choice:'))
 while op != 5:
-    #add
-    if op == 1:
-        print('You choice add!')
-        add = n1 + n2
-        print('The sum of {} and {} are: {}'.format(n1,n2,add))
-    #multiply
-    if op == 2:
-        print('You choice multiply!')
-        mult = n1 * n2
-        print('The multiply of {} and {} are: {}'.format(n1,n2,mult))
-    #bigger
-    if op == 3:
-        if n1 > n2:
-            print('The number {} is bigger than {}'.format(n1,n2))
-        else:
-            print('The number {} is bigger than {}'.format(n2,n1))
+    op = int(input('Type your choice:'))
+#add
+if op == 1:
+    print('You choice add!')
+    add = n1 + n2
+    print('The sum of {} and {} are: {}'.format(n1,n2,add))
+#multiply
+if op == 2:
+    print('You choice multiply!')
+    mult = n1 * n2
+    print('The multiply of {} and {} are: {}!'.format(n1,n2,mult))
+#bigger
+if op == 3:
+    print('You choice bigger!')
+    if n1 > n2:
+        print('The number {} is greater than {}!'.format(n1,n2))
+    elif n2 > n1:
+        print('The number {} is greater than {}!'.format(n2,n1))
+    elif n1 == n2:
+        print('The numbers are the same!')
