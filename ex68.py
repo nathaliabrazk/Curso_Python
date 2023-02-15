0241b14b4e0a23a05156e457fce39e4267b91fa9
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-15 20:11:58 -0300

    .

---
+++ ex68.py
@@ -0,0 +1,6 @@
+#Program will game even or odd with the computer. The game just stop when the user lose, showing the total of 
+#consecutives victories who him conquered in the end of game
+print('=*=' * 20) 
+print('                     LETS PLAY EVEN OR ODD')
+print('=*=' * 20) 
+move = int(input('Type a number:'))
