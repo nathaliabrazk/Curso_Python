82880dbdd1cc9e41e3ae10e067b3349a216e9dba
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-15 10:18:57 -0300

    .

---
+++ ex62.py
@@ -0,0 +1,18 @@
+#Improve the ex 61, asking for the user if him want show more terms. The program end when the user say he 
+#wants show 0 terms
+first = int(input('Type the first term:'))
+reason = int(input('Type the reason:'))
+term = first
+count = 1
+t = 0
+more = 10
+while more != 0:
+    t = t + more
+    while count <= t:
+        print('{} -> '.format(term), end='')
+        term += reason
+        count += 1
+    print('pause')
+    more = int(input('How many terms do you want see: '))
+print('end')
+print('Finished with {} terms shown'.format(t))
