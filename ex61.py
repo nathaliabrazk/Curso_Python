44f9308bee6d6efe0b449ff40df0e1f637adb7c2
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-15 09:39:22 -0300

    .

---
+++ ex61.py
@@ -0,0 +1,8 @@
+#remake the ex 51 who read the first term and the reason of a P.A. In the end show the 10 first terms 
+#of this progression using while
+term = int(input('Type the first term:'))
+reason = int(input('Type the reason:'))
+tenth = 0
+while term < reason:
+    tenth = term * reason
+print('{} -> {}',end= ''.format(term,tenth))
