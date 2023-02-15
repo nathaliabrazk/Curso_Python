15b5eb1928bcba016126054d5d03638c2515c2a7
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-15 19:59:46 -0300

    .

--- ex67.py
+++ ex67.py
@@ -1,11 +1,9 @@
 #Program who show the multiplication table of some numbers, one at a time, for each value typed for the user 
 #The program will stop when the typed number is negative
-n = 0
-l = 0
 while True:
-    print('Type a number to see your multiplication table(for stop the program type a negative number):')
+    n = int(input(('Type a number to see your multiplication table(for stop the program type a negative number):')))
     if n < 0:
         break
     for l in range (1,11):
         print(f'{n} x {l} = {n*l}')
-    
+print('end')        
