487b8d1aea89049d888a9b245a3824a1b31f2bd3
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-14 21:14:17 -0300

    .

--- ex60.py
+++ ex60.py
@@ -1,5 +1,5 @@
 #Program who read a number and show your factorial 
 from math import factorial
 n = int(input('Type the number for show your factorial:'))
-factorial(n)
-print('Calculing {}! = {}'.format(n, factorial))
+f = factorial(n)
+print('Calculing {}! = {}'.format(n, f))
---
+++ ex60b.py
@@ -0,0 +1,7 @@
+#Program who read a number and show your factorial 
+n = int(input('Type the number to show your factorial:'))
+c = n
+while c > 0:
+    print('{}'.format(c))
+    c -= 1
+    
