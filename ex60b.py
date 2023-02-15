21249877cecfbb34a2524be7db200f77c3173bf8
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-14 21:24:06 -0300

    .

--- ex60b.py
+++ ex60b.py
@@ -1,8 +1,11 @@
 #Program who read a number and show your factorial 
 n = int(input('Type the number to show your factorial:'))
 c = n
+f = 1
 print('Calculing {}! = '.format(c), end='')
 while c > 0:
     print('{}'.format(c), end='')
     print(' x ' if c > 1 else ' = ', end='')
+    f *= c
     c -= 1
+print('{}'.format(f))
