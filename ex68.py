5812ffe2fa0ed230b4c001a42a054b20c9eed09e
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-15 21:12:35 -0300

    .

--- ex68.py
+++ ex68.py
@@ -1,4 +1,4 @@
-#Program will game even or odd with the computer. The game just stop when the user lose, showing the total of 
+#Program will game pair or odd with the computer. The game just stop when the user lose, showing the total of 
 #consecutives victories who him conquered in the end of game
 import random 
 total = 0
@@ -6,15 +6,18 @@ win = 0
 lose = 0
 result = 0
 print('=*=' * 20) 
-print('                     LETS PLAY EVEN OR ODD')
+print('                     LETS PLAY PAIR OR ODD')
 print('=*=' * 20) 
-while result != 'WINN':
+while result != 'WIN':
     move = int(input('Type a number:'))
-    op = str(input('You want even or odd?')).strip().upper()
+    op = str(input('You want pair or odd?')).strip().upper()
+    if op != 'PAIR' or op != 'ODD':
+        print('Invalid!\n(type just even or odd)')
     computer = random.randint(0,10)
     print(f'The computer played: {computer}')
     result = move + computer
     print(f'The result are: {result}')
+    print(f'{op}')
     if op == 'PAIR' and result % 2 == 0:
         result = 'WIN'
         win += 1
