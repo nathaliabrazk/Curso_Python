0fb72711ec4ac5f0b2cab30a74432361cafaef04
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-16 23:32:54 -0300

    .

--- ex68.py
+++ ex68.py
@@ -2,33 +2,31 @@
 #consecutives victories who him conquered in the end of game
 import random 
 total = 0
-win = 0
-lose = 0
-result = 0
+victory = 0
+result = ''
 print('=*=' * 20) 
 print('                     LETS PLAY PAIR OR ODD')
 print('=*=' * 20) 
-while result != 'WIN':
+while True:
     user = int(input('Type a number:'))
     op = str(input('You want pair or odd?')).strip().upper()
-    if op != 'PAIR' or op != 'ODD':
-        print('Invalid!\n(type just even or odd)')
     computer = random.randint(0,10)
-    result = user + computer
-    print(f'You play {user} and the computer played: {computer}, result = {result}',end='')
-    print(f' {op}')
+    while result != 'WIN':
+        result = user + computer
+        print(f'You play {user} and the computer played: {computer}, result = {result}',end='')
+        print(f' {op}')
     if op == 'PAIR' and result % 2 == 0:
         result = 'WIN'
-        win += 1
+        victory += 1
         print('USER WIN')
     else:
         print('THE COMPUTER WIN!\nYOU LOSE!')
         break
     if op == 'ODD' and result % 2 == 1:
         result = 'WIN'
-        win += 1
+        victory += 1
         print('USER WIN')
     else:
         print('THE COMPUTER WIN!\nYOU LOSE!')
         break
-print(f'Total of your consecutive victories:{win}')
+print(f'Total of your consecutive victories:{victory}')
