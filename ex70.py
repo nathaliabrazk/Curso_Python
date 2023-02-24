20121530c4a8062dc0bb0fd27eea82d46ddc5ad4
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-24 19:07:08 -0300

    .

--- ex70.py
+++ ex70.py
@@ -7,16 +7,17 @@ total = 0 #will calculate the total expense
 moreproduct = 0 #will store how many products cust more than R$1000
 morecheapprice = 1000 #will store the price of the more expensive product
 morecheapname = '' #will store the name of the more expensive product 
+print('----- MARKETPLACE -----')
 pn = str(input('Type the product name: '))
 price = int(input(('Type the product price R$: ')))
 op = str(input('Continue?: ')).strip().upper()
 while op == 'YES':
-    while op not in ('YESNO'):
+    while op not in ('YESyesNOno'):
         print('invalid!')
-        op = str(input('Type just yes or no:')).strip().upper()
-    pn = str(input('Type the product name:'))
-    price = int(input(('Type the product price R$:')))
-    total = price + total#error 
+        op = str(input('Type just yes or no: ')).strip().upper()
+    print('----- MARKETPLACE -----')
+    pn = str(input('Type the product name: '))
+    price = int(input(('Type the product price R$: ')))
     op = str(input('Continue? ')).strip().upper()
     if price > 1000:
         moreproduct += 1
@@ -25,7 +26,8 @@ while op == 'YES':
             morecheapname = pn
     elif op == 'NO':
         break
-print('end')
+total += price
+print('---------- END ----------')
 print(f'Total expense: R${total}')
 print(f'{moreproduct} products cust more than R$1000')
 print(f'Product name more cheap: {morecheapname} that costs: R${morecheapprice}')
