21dae2848ffb7f7e897a5b8ba1fe85aba88412be
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-23 23:02:29 -0300

    .

--- ex70.py
+++ ex70.py
@@ -5,16 +5,22 @@ op = ''
 price = 0
 total = 0
 moreproduct = 0
-morecheap = 0
-pn = str(input('Type the product name:'))
-price('Type the product price:')
-op = str(input('Continue? ')).strip().upper()
+morecheap = 1000
+pn = str(input('Type the product name: '))
+price('Type the product price: ')
+op = str(input('Continue?(Type yes or no): ')).strip().upper()
 while op == 'YES':
+    while op not in 'YESNO':
+        op = str(input(('Type just yes or no:')))
     pn = str(input('Type the product name:'))
     price('Type the product price:')
     op = str(input('Continue? ')).strip().upper()
     if price > 1000:
         moreproduct += 1
+        if price > morecheap:
+            morecheap = price
+    elif op == 'NO':
+        break
 print('end')
 print(f'Total expense: {total}')
 print(f'{moreproduct} products cust more than R$1000')
