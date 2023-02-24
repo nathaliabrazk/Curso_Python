f9318d6a1b579dd78a31abcd23221ea307a165d0
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-23 23:49:56 -0300

    .

--- ex70.py
+++ ex70.py
@@ -1,22 +1,22 @@
 #Program who read the name and price of a lot products. The program will ask for the user if him want continue. In the 
 #end show: What is the total expense. How many products cust more than R$1000.
 #What is the product name more cheap
-op = ''
-price = 0
-total = 0
-moreproduct = 0
-morecheapprice = 1000
-morecheapname = ''
+op = '' #will store the option (if the user want continue or not)
+price = 0 #will store the price of a product
+total = 0 #will calculate the total expense 
+moreproduct = 0 #will store how many products cust more than R$1000
+morecheapprice = 1000 #will store the price of the more expensive product
+morecheapname = '' #will store the name of the more expensive product 
 pn = str(input('Type the product name: '))
-price = int(input(('Type the product price: ')))
-op = str(input('Continue?(Type yes or no): ')).strip().upper()
+price = int(input(('Type the product price R$: ')))
+op = str(input('Continue?: ')).strip().upper()
 while op == 'YES':
     while op not in ('YESNO'):
         print('invalid!')
         op = str(input('Type just yes or no:')).strip().upper()
     pn = str(input('Type the product name:'))
-    price = int(input(('Type the product price:')))
-    total = total + price
+    price = int(input(('Type the product price R$:')))
+    total = price + total#error 
     op = str(input('Continue? ')).strip().upper()
     if price > 1000:
         moreproduct += 1
@@ -26,6 +26,6 @@ while op == 'YES':
     elif op == 'NO':
         break
 print('end')
-print(f'Total expense: {total}')
+print(f'Total expense: R${total}')
 print(f'{moreproduct} products cust more than R$1000')
-print(f'Product name more cheap: {morecheapname} that costs: {morecheapprice}')
+print(f'Product name more cheap: {morecheapname} that costs: R${morecheapprice}')
