d1b7cf86d2fdd4b426e2ef2112cf1a3653a2274a
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-28 20:01:02 -0300

    .

--- ex75.py
+++ ex75.py
@@ -1,7 +1,7 @@
 #Program wll read 4 numbers and save them in a tuple. At the end show: a)How many times the number 9 appears. 
 #b)In wich position was entered the number 3. c)What were the even numbers
 values = tuple(int(input('Type a number: '))for c in range(1, 5))
-print(f'O numero nove aparece {values.count(9)} vezes')
-print(f'Valor 3 foi digitado pela primeira vez na {values.index(3)+1}º posição' if 3 in values else 'Não foi digitado valor 3')
-print('Valores pares digitados foram', end=' ')
+print(f'The number 9 apears {values.count(9)} times')
+print(f'The value 3 was typed for the first on {values.index(3)+1}º position' if 3 in values else 'The value 3 was not typed')
+print('The even values entered', end=' ')
 print({n for n in values if n % 2 == 0}, end=' ')
