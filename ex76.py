6c08dc100336a1786fe0bbe6f6608aeb595989c7
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-28 20:26:17 -0300

    .

---
+++ ex76.py
@@ -0,0 +1,5 @@
+#Program will have a single tuple that stores product names with their respective values. Then show 
+#a list of prices, organizing the data on tabular form
+products = ('Milk', 3.0, 'Bread', 0.50, 'Orange juice', 2.0, 'Strawberry pie', 4.0, 'Grape jelly', 10.0, 
+            'Honey', 6.0, 'Cocunut cake', 2.50, 'Lemon cake', 2.50, 'Orange cake', 2.50, 'Party cake', 20.0)
+print(products[3])
