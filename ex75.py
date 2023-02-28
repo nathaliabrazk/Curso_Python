01c84d13985c96eb50f706dd06417c05fbf7a977
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-02-27 22:45:05 -0300

    .

---
+++ ex75.py
@@ -0,0 +1,4 @@
+#Program wll read 4 numbers and save them in a tuple. At the end show: a)How many times the number 9 appears. 
+#b)In wich position was entered the number 3. c)What were the even numbers
+for c in range(4):
+    n  = int(input('Type a number: '))
