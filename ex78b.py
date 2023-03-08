07549bbdd2fa24aee7a47b5826368b07ae39750f
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-07 22:36:25 -0300

    .

--- ex78b.py
+++ ex78b.py
@@ -1,15 +1,26 @@
 #Program will read 5 numerical values and save on a list. On the end then show the bigger and the smallest value 
 #and yours positions on the list
-list = []
-big = 0
-small = 0
+listvalues = []
+big = small = 0
+#repeat loop to fill the list 
 for c in range(0,5):
-    list.append(int(input(f'Type the numerical value to position {c}:')))
+    listvalues.append(int(input('Type a value:')))
     if c == 0:
-        big = small = list[c]
+        big = small = listvalues[0]
     else:
-        if list > big:
-            big == list[c]
-        if list < small:
-            small == list[c]
-print(f'The values of a list: {list}')
+        if listvalues[c] > big:
+            big = listvalues[c]
+        if listvalues[c] < small:
+            small = listvalues[c]
+print(f'You type the values: {listvalues}')
+print(f'The SMALLEST values: on positions:',end='')
+#repeat loop to find the positions from the smallest values
+for i, v in enumerate(listvalues):
+    if v == small:
+        print(f'{i}...', end='')
+
+print(f'The HIGGESTS values: {big} on positions:',end='')
+#repeat loop to find the positions from the higgests values
+for i, v in enumerate(listvalues):
+    if v == big:
+        print(f'{i}...', end='')
