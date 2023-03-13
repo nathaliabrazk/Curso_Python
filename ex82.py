83d8b16f5618e26b48f274abfd76369a2aaba423
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-12 21:44:00 -0300

    .

---
+++ ex82.py
@@ -0,0 +1,5 @@
+#Program will read several numbers and put them in a list, after that it will make 2 extra lists that will have
+#only even values and odd values respectively. At the end show the content of the 3 lists
+value = []
+while True:
+    value.append('Type the number:')
