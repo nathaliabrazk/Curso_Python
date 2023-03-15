1c7a701d962e9f3026cf189d6b9211283212c0fd
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-15 17:46:49 -0300

    .

--- aula18e.py
+++ aula18e.py
@@ -5,5 +5,11 @@ for c in range(0,2):
     data.append(str(input('Name:')))
     data.append(int(input('Age:')))
     group.append(data[:])#making a copy on original list 
-    
+    data.clear()
 print(group)
+#looping with condition
+for p in group:
+    if p[1] >= 21:
+        print(f'{p[0]} Is of legal age!')
+    else:
+        print(f'{p[0]} Is not of legal age')
