8183753dd87321ffc08f68b51978748d395946cb
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-17 20:00:40 -0300

    .

--- ex84.py
+++ ex84.py
@@ -2,9 +2,34 @@
 #A)How many people were registered. b)A list with the heavier people. c)A list with the more skinny people
 temporary = []
 principal = []
+big = small = 0
 while True:
-    temporary.append(str(input('name:')))
-    temporary.append(float(input('weight:')))
+    temporary.append(str(input('name:')))#gonna be registered in position 0
+    temporary.append(float(input('weight:')))#gonna be registered in position 1
+    if len(principal) == 0:
+        big = smal = temporary[1]
+    else:
+        if temporary[1] > big:
+            big = temporary[1]
+        if temporary[1] < small:
+            small = temporary[1]
+    principal.append(temporary[:])#[:] is for copy all the respository
+    temporary.clear()#clear() its for init a list when the loop finish
     op = str(input('Continue?')).strip().upper()
     if op in 'NO':
         break
+print('-' * 30)
+print('            RESULT' )
+print('-' * 30)
+print('Typed datas: ',principal)
+print('Total of people: ',len(principal))
+print(f'The biggest weight was : {big} kg from: ',end='')
+for p in principal:#loop for show the name of people
+    if p[1] == big:
+        print(f'[{p[0]}]')
+print('')
+print(f'The smallest weight was : {small} kg from: ',end='')
+for p in principal:#loop for show the name of people
+    if p[1] == small:
+        print(f'[{p[0]}]')
+print('')
