112dcaba8fac6957d2eac95864531c2833ad0d88
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-21 12:30:46 -0300

    .

--- ex89.py
+++ ex89.py
@@ -2,13 +2,27 @@
 #At the end, it shows a bulletin with the average of each one and allows the user to show the grade 
 #of each student individually
 regist = list()
-
-name = str(input('Name: '))
-note1 = int(input('1º note:'))
-note2 = int(input('2º note:'))
-average = (note1 + note2) / 2
-regist.append([name, [note1, note2], average])
+op = ''
+while True:
+    name = str(input('Name: '))
+    note1 = float(input('1º note:'))
+    note2 = float(input('2º note:'))
+    average = (note1 + note2) / 2
+    regist.append([name, [note1, note2], average])
+    op = str(input('Continue?')).upper()
+    if op == 'NO':
+        break
 print('-=' * 30)
-print('No.      NAME        AVERAGE')
-print(regist)
-print('-' * 30)
+print(f'{"No.":<4}{"NAME":<10}{"AVERAGE":>8}')
+print('-' * 26)
+for i, a in enumerate(regist):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-' * 35)
+    query = int(input('You want see the notes of what student(999 stop the program): '))
+    if query == 999:
+        print('FINISHING...')
+        break
+    if query <= len(regist) - 1:
+        print(f'Notes from {regist[query][0]} they are: {regist[query][1]}')
+print('<<<END>>>')
