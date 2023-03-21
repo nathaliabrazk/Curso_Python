d254508567cf003d965aa9efde63dd9f12f15c40
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-21 11:23:31 -0300

    .

--- ex89.py
+++ ex89.py
@@ -1,9 +1,14 @@
 #Program will read one name and two notes from several students and regist all in a compound list. 
 #At the end, it shows a bulletin with the average of each one and allows the user to show the grade 
 #of each student individually
-note = []
-name = str(input('Name:'))
-for c in range(0,2):
-    note = int(input(f'{c} Note:'))
-print(f'Name: {name}')
-print(f'Note: {note}')
+regist = list()
+
+name = str(input('Name: '))
+note1 = int(input('1º note:'))
+note2 = int(input('2º note:'))
+average = (note1 + note2) / 2
+regist.append([name, [note1, note2], average])
+print('-=' * 30)
+print('No.      NAME        AVERAGE')
+print(regist)
+print('-' * 30)
