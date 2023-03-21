a08375d9a2ae627bc06beceb8b941d0cb8b46591
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-21 16:56:31 -0300

    .

---
+++ aula19.py
@@ -0,0 +1,9 @@
+#DICIONARY
+data = dict()
+data = {'name': 'Pedro', 'age': 25} 
+print(data['name'])
+print(data['age'])
+data['sexo'] = 'M'#to add a element 
+print(data['sexo'])
+del data['age']#to delete a element
+print(data['age'])
---
+++ aula19b.py
@@ -0,0 +1,10 @@
+#DICIONARY
+        #element = tittle called = Star Wars
+film = {'Title': 'Star Wars', 
+        'year': 1977,
+        'Director': 'George Lucas'
+}
+print(film.values())#value() gonna show all the 'higher' part
+print(film.keys())#keys() gonna show all the 'bottom' part
+print(film.items())#items() gonna show all items from the dicionary
+#VALUE| KEY | ITEM
