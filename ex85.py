8ac21e943bb878a7deb0022c8e7c88e9362a4003
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-20 21:45:17 -0300

    .

---
+++ ex85.py
@@ -0,0 +1,14 @@
+#Program in wich the user can type 7 number values and regist them in a single list and separete the pair numbers 
+#and the odd numbers. At the end show yhe pair and the odd numbers in ascending order
+#     0    1
+n = [ [], [] ]
+value = 0
+for c in range(1,8):
+    value = int(input(f'Type the {c}º value: '))
+    if value % 2 == 0:
+        n[0].append(value)
+    else:
+        n[1].append(value)
+print('+=' * 30)
+print(f'Pair values: {n[0]}\nOdd values:{n[1]}')
+  
