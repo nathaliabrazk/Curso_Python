c91020042d42168fcd4e0fe13ca81c2a4ce9b63e
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-20 22:09:54 -0300

    .

---
+++ ex86.py
@@ -0,0 +1,10 @@
+#Program that create a matriz 3x3 with typed values. At the end show on the screen the matriz
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range (0, 3):
+    for c in range (0, 3):
+        matriz[l][c] = int(input(f'Type a value to [{l}, {c}]:'))
+print('====== MATRIZ ======')
+for l in range (0,3):
+    for c in range(0,3):
+        print(f'|{matriz[l][c]:5^}|',end='')
+    print()
