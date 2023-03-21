b97c26baaf5dc760713df03c1074798401f4865f
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-21 17:23:37 -0300

    .

---
+++ aula19c.py
@@ -0,0 +1,8 @@
+#DICIONARY
+#
+film = {'Title': 'Star Wars', 
+        'year': 1977,
+        'Director': 'George Lucas'
+}
+for k, v in film.items():
+    print(f'The {k} is {v}')
---
+++ aula19d.py
@@ -0,0 +1,9 @@
+#DICIONARY
+MovieRental = list()
+film = {'film': 'Star Wars', 'year': 1977 ,'director': 'George Lucas'
+}
+MovieRental.append(film)
+film = {'film': 'Avengers', 'Year': 2012, 'director': 'Josh Whedon'}
+MovieRental.append(film)
+
+print(MovieRental[0])
