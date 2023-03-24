b762ace771b876700ea19815586bf7c432acb778
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-23 22:16:29 -0300

    .

--- ex93.py
+++ ex93.py
@@ -1,1 +1,11 @@
 #DICIONARY
+#Program that manage a use of a football player. The program will read the soccer player name and 
+#how much matches him played. After will read the quantidy of goals made in each matches. After
+#all this will be saved on a dicionary, including the total of goals madet during a champeonship
+regist = dict()#dicionary
+regist['name'] = str(input('Name:'))
+regist['matches'] = int(input('Quanty of matches:'))
+for c in range (regist['matches']):
+    regist['goal'] = int(input('Quanty of goals: '))
+print('-=' * 30)
+print(f'The player of soccer {regist["name"]} play {regist["matches"]} matches')
