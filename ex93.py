7af806cf5c23038dcc94b1131958af2f6018af0d
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-28 22:50:35 -0300

    .

--- ex93.py
+++ ex93.py
@@ -2,10 +2,14 @@
 #Program that manage a use of a football player. The program will read the soccer player name and 
 #how much matches him played. After will read the quantidy of goals made in each matches. After
 #all this will be saved on a dicionary, including the total of goals madet during a champeonship
-regist = dict()#dicionary
-regist['name'] = str(input('Name:'))
-regist['matches'] = int(input('Quanty of matches:'))
-for c in range (regist['matches']):
-    regist['goal'] = int(input('Quanty of goals: '))
-print('-=' * 30)
-print(f'The player of soccer {regist["name"]} play {regist["matches"]} matches')
+player = dict()
+matches = list()
+player['name'] = str(input('Name:'))
+totalMatches = int(input(f'How many matches the player {player["name"]} it played?'))
+for c in range(1, totalMatches):
+    player['gols'] = int(input(f'How many gols the player {player["name"]} did on matche ({c}):'))
+    player['count'] = sum(matches)
+print('=-' * 30)
+print(f'Ther player {player["name"]} play {len(player["gols"])} matches')
+for i, v in enumerate(player['gols']):
+    print(f'=> On match {i} did {v} gols')
