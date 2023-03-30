c482afaddae1c7fd646b096b51f07f328d45f336
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-29 22:27:16 -0300

    .

--- ex94.py
+++ ex94.py
@@ -1,30 +1,30 @@
 #DICIONARY
+#LIST OF DICIONARYS
 #The program will read the name, age, sex of a several people keeping the datas of each person on a dicionary
 #and all the dicionarys on a list. Show:
 #a)How much people were registered *
 #b)The avarage of age from the group 
-#c)A list with all the womans
+#c)A list with all the womans*
 #d)A list with all the people with age above average 
 data = dict()
+allpeople = list()
 op = ''
-woman = list()
-quant = 0
-avarage = 0
-while True:
+while True: 
+    allpeople.clear()
     data['name'] = str(input('Name: '))
+    while True:
+        data['sex'] = str(input('Sex [F/M]')).strip().upper()
+        if data['sex'] in 'FM':
+            break
+        print('ERROR! Type just [F/M]')
     data['age'] = int(input('Age: '))
-    data['sex'] = str(input('Sex [F/M]: '))
-    quant += 1
-    if data['sex'] not in 'FM':
-        data['sex'] = str(input('Type just F or M: '))
-    if data['sex'] in 'F':
-        woman.append(data)
-    op = str(input('continue? ')).strip().upper()
-    if op in 'NOno':
-        break
-avarage = sum(data['age']) / quant
-print('-=' * 30)
-print(f'Quantidy of people registered: {quant}')
-print(f'The avarage of age from the people: {avarage}')
-print(f'List with the womans:\n{woman}')
-#print(f'List of people with age above average:\n{}')
+    allpeople.append(data.copy())
+    while True:
+        op = str(input('Continue? ')).upper()[0]
+        if op in 'YESNO':
+            break 
+        print('ERROR! Type just yes or no')
+        if op == 'NO':
+            break 
+    print('-=')
+    print(allpeople)
