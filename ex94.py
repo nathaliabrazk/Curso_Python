5a58b29ca030d5509ae42f9e49df4860a3c0a3b4
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-03-29 22:56:40 -0300

    .

--- ex94.py
+++ ex94.py
@@ -6,25 +6,31 @@
 #b)The avarage of age from the group 
 #c)A list with all the womans*
 #d)A list with all the people with age above average 
-data = dict()
-allpeople = list()
-op = ''
-while True: 
-    allpeople.clear()
-    data['name'] = str(input('Name: '))
+galera = list()
+pessoa = dict()
+soma = media = 0
+while True:
+    pessoa['nome'] = str(input('Nome:'))
     while True:
-        data['sex'] = str(input('Sex [F/M]')).strip().upper()
-        if data['sex'] in 'FM':
+        if pessoa['sexo'] in 'MF':
+            break 
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()
+        if resp in 'SN':
             break
-        print('ERROR! Type just [F/M]')
-    data['age'] = int(input('Age: '))
-    allpeople.append(data.copy())
-    while True:
-        op = str(input('Continue? ')).upper()[0]
-        if op in 'YESNO':
-            break 
-        print('ERROR! Type just yes or no')
-        if op == 'NO':
-            break 
-    print('-=')
-    print(allpeople)
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+    print('-=' * 30)
+    print(f'Ao todo temos {len(galera)} pessoas cadastradas')
+    media = soma / len(galera)
+    print(f'A media de idade é de {media:5.2f} anos')
+    print(f'As mulheres cadastradas foram ', end='')
+    for p in galera:
+        if p['sexo'] in 'Ff':
+            print(f'{p["nome"]}', end='')
+    print()
+    print('D) Listas das pessoas que estão acima da média: ', end='' )
