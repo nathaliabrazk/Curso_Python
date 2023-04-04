15f77f062c1b56d6396f9975936166653d1eb68d
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-04-04 18:09:05 -0300

    .

--- ex94.py
+++ ex94.py
@@ -6,31 +6,12 @@
 #b)The avarage of age from the group 
 #c)A list with all the womans*
 #d)A list with all the people with age above average 
-galera = list()
-pessoa = dict()
-soma = media = 0
+allPeople = list()
+people = dict()
+sum = average = 0
+people['name'] = str(input('Name: '))
 while True:
-    pessoa['nome'] = str(input('Nome:'))
-    while True:
-        if pessoa['sexo'] in 'MF':
-            break 
-        print('ERRO! Por favor, digite apenas M ou F.')
-    pessoa['idade'] = int(input('Idade: '))
-    soma += pessoa['idade']
-    while True:
-        resp = str(input('Quer continuar? [S/N] ')).upper()
-        if resp in 'SN':
-            break
-        print('ERRO! Responda apenas S ou N.')
-    if resp == 'N':
+    people['sex'] = str(input('Sex [F/M]: ')).upper()[0]#[0] to get just the first letter
+    if people['sex'] in 'MF':
         break
-    print('-=' * 30)
-    print(f'Ao todo temos {len(galera)} pessoas cadastradas')
-    media = soma / len(galera)
-    print(f'A media de idade é de {media:5.2f} anos')
-    print(f'As mulheres cadastradas foram ', end='')
-    for p in galera:
-        if p['sexo'] in 'Ff':
-            print(f'{p["nome"]}', end='')
-    print()
-    print('D) Listas das pessoas que estão acima da média: ', end='' )
+    print('ERROR - Type just [F or M]')
