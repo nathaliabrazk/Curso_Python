2cc6638b9de8adc7d2608a202637acacc1fa4e7c
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-04-04 18:44:58 -0300

    .

--- ex94.py
+++ ex94.py
@@ -8,10 +8,24 @@
 #d)A list with all the people with age above average 
 allPeople = list()
 people = dict()
-sum = average = 0
-people['name'] = str(input('Name: '))
-while True:
-    people['sex'] = str(input('Sex [F/M]: ')).upper()[0]#[0] to get just the first letter
-    if people['sex'] in 'MF':
+while True:#This while True it suits to regist all datas from dictionaryes on a list
+    people.clear()#to reset the list and add a new user
+    people['name'] = str(input('Name: '))
+    while True:
+        #validation
+        people['sex'] = str(input('Sex [F/M]: ')).strip().upper()[0]#[0] to get just the first letter
+        if people['sex'] in 'MF':
+            break
+        print('ERROR - Type just [F or M]')
+    people['age'] = int(input('Age: '))
+    allPeople.append(people.copy())#to regist a copy from the dicionary on a list
+    while True:
+        op = str(input('Continue [Y/N]? ')).strip().upper()[0]# [0] to get just the first letter 
+        if op in 'YN':
+            break
+        print('Type just [Y or N]')
+    if op in 'N':
         break
-    print('ERROR - Type just [F or M]')
+print('-=' * 30)
+print(f'Quanty of registered people: {len(allPeople)}')
+
