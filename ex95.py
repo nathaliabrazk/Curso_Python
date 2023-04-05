0048759c1e86063099c6a7e8038bbcecc4455405
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-04-05 19:34:38 -0300

    .

--- ex95.py
+++ ex95.py
@@ -3,19 +3,30 @@
 #of the use of each player
 player = dict()
 match = list()
+details = list()
 op = ''
 while True: 
+    player.clear()
     player['nome'] = str(input('Player name: '))
     tot = int(input(f'How much matches {player["nome"]} it played? '))
+    match.clear()
     for c in range(0, tot):
         match.append(int(input(f'How much gols on match {c+1}: ')))
     player['gols'] = match[:]
     player['total'] = sum(match)
-    op = str(input('Continue? ')).strip().upper()
-    if op != 'NO':
+    details.append(player.copy())
+    while True:
+        op = str(input('Continue? ')).strip().upper()
+        if op in 'YESNO':
+            break
+        print('Type just YES or NO!').strip().upper()
+    if op == 'NO':
         break
-    print('-=' * 30)
-    print(f'The player {player["nome"]} played {len(player["gols"])} matches')
-    for i,v in enumerate(player['gols']):
-        print(f'  => On match {i+1}, did {v} gols')
-    print(f'Total of {player["total"]} gols')
+
+print('-' * 40)
+for k, v in enumerate(details):
+    print(f'{k:>4}', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
