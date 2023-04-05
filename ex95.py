46d4487de16e15dfac4f356db5d801a29546dfc9
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-04-05 20:12:35 -0300

    .

--- ex95.py
+++ ex95.py
@@ -5,10 +5,11 @@ player = dict()
 match = list()
 details = list()
 op = ''
+search = 0
 while True: 
     player.clear()
-    player['nome'] = str(input('Player name: '))
-    tot = int(input(f'How much matches {player["nome"]} it played? '))
+    player['name'] = str(input('Player name: '))
+    tot = int(input(f'How much matches {player["name"]} it played? '))
     match.clear()
     for c in range(0, tot):
         match.append(int(input(f'How much gols on match {c+1}: ')))
@@ -37,3 +38,14 @@ for k, v in enumerate(details):
         print(f'{str(d):<15}', end='')
     print()
 print('-' * 40)
+#search 
+while True:
+    search = int(input('Type the player code to show datas (type 999 to stop the program): '))
+    if search == 999:
+        break
+    if search >= len(details):
+        print(f'ERROR |Dont exist player with code {search}|')
+    else:
+        print(f'-- DETAILS OF THE PLAYER {details[search]["name"]}:')
+        for i, g in enumerate(details[search]['gols']):
+            print(f'--> On match {i} did {g + 1} gols')
