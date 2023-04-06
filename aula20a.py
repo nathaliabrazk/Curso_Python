7fa8178ff12570fa1a9a5f88d5f1d1678c8c5add
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-04-05 21:40:07 -0300

    .

---
+++ aula20a.py
@@ -0,0 +1,7 @@
+#FUNCTIONS
+def sum (a, b):
+    s = a + b
+    print(s)
+sum(4, 5)
+sum(8, 9)
+sum(2, 1)
---
+++ ex96.py
@@ -0,0 +1,14 @@
+#FUNCTION
+def mostralinha():
+    print('-----------------------------------------')
+
+mostralinha()
+print('             STUDENTS SISTEM')
+mostralinha()
+mostralinha()
+print('        REGISTRATION OF EMPLOYEE')
+mostralinha()
+mostralinha()
+print('             SYSTEM ERROR')
+mostralinha()
+mostralinha()
