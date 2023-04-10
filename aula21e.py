5914d0278e196ce8f1c28575fec625a117ec57c7
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-04-10 17:42:23 -0300

    .

--- aula21e.py
+++ aula21e.py
@@ -1,2 +1,19 @@
 #FUNCTIONS PT 2
 #Scope of variables 
+
+#function
+def test(b):
+    global a #Global variable being used inside the function
+    a = 8 #Other variable with name a but local, not global 
+    b += 4
+    c = 2
+    print(f'a inside equals to {a}')#Local variable
+    print(f'b inside equals to {b}')#Local variable
+    print(f'c inside equals to {c}')#Local variable
+#main
+
+a = 5#Global variable 
+test(a)
+print(f'a outsize equals to {a}')#Global variable 
+print(f'b outsize equals to {b}')#Local variable
+print(f'c outsize equals to {c}')#Local variable
