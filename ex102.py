01f1b0e8efaaa38320c915426e5eb3c9793c1e0f
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-04-10 19:07:26 -0300

    .

---
+++ ex102.py
@@ -0,0 +1,20 @@
+#FUNCTION PT 2
+#Program who has a function calling factorial() who recives two parameters: the first who indicate  the number
+#to calculate and other calling show, who gonna be optional who indicate if be showed or not on the screen
+#the process of factorial calculation
+
+#Function
+def factorial(n, show=False):
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(f'{c} x ', end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+
+#main
+print(factorial(5, show=True))
