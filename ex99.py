45fc89dac32766396e0b607d3d4cd301a511fbdd
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-04-09 23:03:31 -0300

    .

--- ex99.py
+++ ex99.py
@@ -1,6 +1,8 @@
 #FUNCTION
 #Program who has a function calling bigger(), who receive several intenger values. The program will analize 
 #all the values and say wich one is bigger
+from time import sleep
+
 
 #function
 def bigger(* values):
@@ -8,11 +10,16 @@ def bigger(* values):
     print('Analizing the past values...')
     for number in values:
         print(f'{number} ', end='')
-
+        sleep(0.3)
+        if count == 0:
+            bigger = number
+        else:
+            if number > bigger:
+                bigger = number
+        count += 1
+    print(f'Were informed: {count} values.')
+    print(f'The biggest value informed he was: {bigger}.')
 #main
-bigger()
-bigger()
-bigger()
-bigger = int(input('Type several values to know wich one is bigger: '))
+bigger(10, 5, 2, 60, 3, 1)
 
 
