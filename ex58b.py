2c37fb937eab31598bd084139d96818d6b04db18
Nathália Braz Krawczyk <nathaliabrazk@gmail.com>
2023-06-06 22:55:57 -0300

    Update ex58b.py

--- ex58b.py
+++ ex58b.py
@@ -2,6 +2,7 @@
 #Improve the ex 28 where the computer go "think" in a number between 1 and 10. But now the user go try guess
 #until hit, showing in the end how many guesses were needed to get yhe right answer
 from random import randint
+from time import sleep
 computer = randint(0,10)
 print('HELLO, Im your computer!\nIm thinking in a number between 1 and 10')
 hit = False
@@ -13,7 +14,10 @@ while not hit:
         hit = True
     else:
         if user < computer:
+            sleep(0.5)
             print('More...Try again.')
         elif user > computer:
+            sleep(0.5)
             print('Less...Try again.')
+sleep(0.8)
 print('YOU WIN!\nNumber of try you needed to win:{}'.format(t))
